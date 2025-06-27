38e947810c6c224ee8643a68c48e864529177c53
Juan Manuel Romero Ruiz <theskills131@gmail.com>
2025-06-27 18:31:25 +0200

    Cambios varios

--- src/main/java/aiss/PIntegracion/model/User.java
+++ src/main/java/aiss/PIntegracion/model/User.java
@@ -70,34 +70,13 @@ public class User {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(User.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("id");
-        sb.append('=');
-        sb.append(((this.id == null)?"<null>":this.id));
-        sb.append(',');
-        sb.append("username");
-        sb.append('=');
-        sb.append(((this.username == null)?"<null>":this.username));
-        sb.append(',');
-        sb.append("name");
-        sb.append('=');
-        sb.append(((this.name == null)?"<null>":this.name));
-        sb.append(',');
-        sb.append("avatarUrl");
-        sb.append('=');
-        sb.append(((this.avatarUrl == null)?"<null>":this.avatarUrl));
-        sb.append(',');
-        sb.append("webUrl");
-        sb.append('=');
-        sb.append(((this.webUrl == null)?"<null>":this.webUrl));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
-    }
 
+        return "User{" +
+                "id = " + id +
+                ", username = " + username +
+                ", name = " + name +
+                ", avatarUrl = " + avatarUrl +
+                ", webUrl = " + webUrl +
+                "}";
+    }
 }
