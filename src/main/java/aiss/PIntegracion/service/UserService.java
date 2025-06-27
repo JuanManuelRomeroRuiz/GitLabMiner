c8fe8f587378f25efa87dbe99f8396e9b3d00555
Juan Manuel Romero Ruiz <theskills131@gmail.com>
2025-06-27 18:23:52 +0200

    Arreglo Service Users

--- src/main/java/aiss/PIntegracion/service/UserService.java
+++ src/main/java/aiss/PIntegracion/service/UserService.java
@@ -2,7 +2,10 @@ package aiss.PIntegracion.service;
 
 import aiss.PIntegracion.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,19 +16,25 @@ import java.util.List;
 @Service
 public class UserService {
 
+    @Value("${gitlab.token}")
+    private String token;
+
     @Autowired
     RestTemplate restTemplate;
 
     public List<User> getUsers() {
         String url = "https://gitlab.com/api/v4/users";
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("PRIVATE-TOKEN", token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
         ResponseEntity<List<User>> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
-                null,
+                entity,
                 new ParameterizedTypeReference<List<User>>() {}
         );
-
         return response.getBody();
     }
 }
